dd84c7ea57d9ac5a8c2f8d051d1349d51cba7222
sgreeff <12202853+sgreeff@users.noreply.github.com>
2025-12-19 22:54:17 +0000

    feat: implement alphabet shifting logic using list slicing

---
+++ 01-Basics/caesar_cipher.py
@@ -0,0 +1,7 @@
+"""freecodecamp.org worshop - Build a Caesar Cipher """
+
+#implement alphabet shifting logic using list slicing
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+shift = 5
+shifted_alphabet = alphabet[shift:] + alphabet[0:shift]
+print(shifted_alphabet)
