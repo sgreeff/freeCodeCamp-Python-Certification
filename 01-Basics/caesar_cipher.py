03125443d19c821158541326ff3a90f04ac648df
sgreeff <12202853+sgreeff@users.noreply.github.com>
2025-12-19 23:30:53 +0000

    feat: making caesar reusable

--- 01-Basics/caesar_cipher.py
+++ 01-Basics/caesar_cipher.py
@@ -1,17 +1,18 @@
 """freecodecamp.org worshop - Build a Caesar Cipher """
 
-#alphabet shifting logic using list slicing
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-shift = 5
-shifted_alphabet = alphabet[shift:] + alphabet[0:shift]
+def caesar():
+    #alphabet shifting logic using list slicing
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    shift = 5
+    shifted_alphabet = alphabet[shift:] + alphabet[0:shift]
 
-#character mapping using str.maketrans for optimised translation
-translation_table = str.maketrans(alphabet, shifted_alphabet)
+    #character mapping using str.maketrans for optimised translation
+    translation_table = str.maketrans(alphabet, shifted_alphabet)
 
-#text to encrypt
-text = "hello world"
+    #text to encrypt
+    text = "hello world"
 
-#using the translate () funtion to return a copy of the original string 
-#where the characters have been replaced based on the translation table
-encrypted_text = text.translate(translation_table)
-print(encrypted_text)
+    #using the translate () funtion to return a copy of the original string 
+    #where the characters have been replaced based on the translation table
+    encrypted_text = text.translate(translation_table)
+    print(encrypted_text)
