e6bd4e98a2bb2c2cfb9c436825fa3ade58ab2087
sgreeff <12202853+sgreeff@users.noreply.github.com>
2025-12-19 23:34:40 +0000

    feat: create caesar(text, shift) function for dynamic encryption

--- 01-Basics/caesar_cipher.py
+++ 01-Basics/caesar_cipher.py
@@ -1,18 +1,17 @@
 """freecodecamp.org worshop - Build a Caesar Cipher """
 
-def caesar():
+def caesar(text, shift):
     #alphabet shifting logic using list slicing
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    shift = 5
     shifted_alphabet = alphabet[shift:] + alphabet[0:shift]
 
     #character mapping using str.maketrans for optimised translation
     translation_table = str.maketrans(alphabet, shifted_alphabet)
 
-    #text to encrypt
-    text = "hello world"
-
     #using the translate () funtion to return a copy of the original string 
     #where the characters have been replaced based on the translation table
     encrypted_text = text.translate(translation_table)
     print(encrypted_text)
+
+#caesar function call
+caesar('hello world',5)
