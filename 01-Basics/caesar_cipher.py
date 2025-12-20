0cd1e0c9ad1454132ae1fca748b1d50bc3306774
sgreeff <12202853+sgreeff@users.noreply.github.com>
2025-12-20 19:29:54 +0000

    feat: more validation for shift to be possitive. Also added comments for validaiton.

--- 01-Basics/caesar_cipher.py
+++ 01-Basics/caesar_cipher.py
@@ -2,17 +2,22 @@
 
 def caesar(text, shift):
     if not isinstance(shift, int):
+        #validation for shift to be an integer
         return 'Shift must be an integer value.'
-    #alphabet shifting logic using list slicing
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    shifted_alphabet = alphabet[shift:] + alphabet[0:shift]
+    elif shift < 1:
+        #validation for shift to be postive
+        return 'Shift must be a positive integer.'
+    else:
+        #alphabet shifting logic using list slicing
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        shifted_alphabet = alphabet[shift:] + alphabet[0:shift]
 
-    #character mapping using str.maketrans for optimised translation
-    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+        #character mapping using str.maketrans for optimised translation
+        translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
 
-    #using the translate () funtion to return a copy of the original string 
-    #where the characters have been replaced based on the translation table
-    return text.translate(translation_table)
+        #using the translate () funtion to return a copy of the original string 
+        #where the characters have been replaced based on the translation table
+        return text.translate(translation_table)
 
 #caesar function call
 print(caesar('hello world',5))
