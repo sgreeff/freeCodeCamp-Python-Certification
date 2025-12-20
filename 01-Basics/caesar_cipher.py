0fa9906d5d09f9b890d8364aa89ca316e3f89876
sgreeff <12202853+sgreeff@users.noreply.github.com>
2025-12-20 19:21:57 +0000

    feat: adding validation for shift to be an integer

--- 01-Basics/caesar_cipher.py
+++ 01-Basics/caesar_cipher.py
@@ -1,12 +1,14 @@
 """freecodecamp.org worshop - Build a Caesar Cipher """
 
 def caesar(text, shift):
+    if isinstance(shift, int):
+        return 'Shift must be an integer value.'
     #alphabet shifting logic using list slicing
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     shifted_alphabet = alphabet[shift:] + alphabet[0:shift]
 
     #character mapping using str.maketrans for optimised translation
-    translation_table = str.maketrans(alphabet, shifted_alphabet)
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
 
     #using the translate () funtion to return a copy of the original string 
     #where the characters have been replaced based on the translation table
