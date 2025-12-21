7f20f3068d90f4509790b99fc2904061659c8cc6
sgreeff <12202853+sgreeff@users.noreply.github.com>
2025-12-21 04:07:30 +0000

    feat: add character name validation logic

--- 01-Basics/rgb_character.py
+++ 01-Basics/rgb_character.py
@@ -2,4 +2,12 @@ def create_character(character_name, strength, intelligence, charisma):
     full_dot = '●'
     empty_dot = '○'
 
+    if not isinstance(character_name,str):
+        return 'The character name should be a string'
+    if character_name == "":
+        return 'The character should have a name'
+    if len(character_name) > 10:
+        return 'The character name is too long'
+    if " " in character_name:
+        return 'The character name should not contain spaces'
     
